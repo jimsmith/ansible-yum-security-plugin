1df572325fcb4b5cfffffb80600ad1e80f481c81
Barney Hanlon <barney@shrikeh.net>
2015-09-21 15:23:07 +0100

    Update test-role.sh for bash

--- tests/test-role.sh
+++ tests/test-role.sh
@@ -2,26 +2,30 @@
 
 test_role() {
 
+  local DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)";
+
+  cd ${DIR};
+
   local TEST_ANSIBLE_INSTALLER_URI='https://raw.githubusercontent.com/shrikeh/ansible-virtualenv/master/init.sh';
   local TEST_ANSIBLE_VENV='.venv';
   local TEST_ANSIBLE_VERSION='v1.9.2-1';
   local TEST_ANSIBLE_CHECKOUT_PATH='./.ansible';
   local TEST_ANSIBLE_INVENTORY_FILE='./inventory';
-  local TEST_ANSIBLE_PLAYBOOK='playbook.yml';
+  local TEST_ANSIBLE_PLAYBOOK='./playbook.yml';
   local TEST_REMOTE_USER='root';
 
-  . <(curl -L --silent "$TEST_ANSIBLE_INSTALLER_URI") \
-    -d "$TEST_ANSIBLE_CHECKOUT_PATH" \
-    --branch "$TEST_ANSIBLE_VERSION" \
-    --venv "$TEST_ANSIBLE_VENV" \
+  . <(curl -L --silent "${TEST_ANSIBLE_INSTALLER_URI}") \
+    -d "${TEST_ANSIBLE_CHECKOUT_PATH}" \
+    --branch "${TEST_ANSIBLE_VERSION}" \
+    --venv "${TEST_ANSIBLE_VENV}" \
     --use-pip-version \
   ;
 
-  ( ansible-playbook -i "$TEST_ANSIBLE_INVENTORY_FILE" \
-    "$TEST_ANSIBLE_PLAYBOOK" \
-    -u "$TEST_REMOTE_USER" \
+  ( ansible-playbook -i "${TEST_ANSIBLE_INVENTORY_FILE}" \
+    "${TEST_ANSIBLE_PLAYBOOK}" \
+    -u "${TEST_REMOTE_USER}" \
     -vvvv \
-    --extra-vars "test_ip=$TEST_IP" \
+    --extra-vars "test_ip=${TEST_IP}" \
   );
 }
 
